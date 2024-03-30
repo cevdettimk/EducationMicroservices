c886956895bf5ab69dfedcfc7865f5ea1047411b
cevdet.timk <cevdet.timk@DT828>
2024-03-30 23:47:21 +0300

    Added fake payment service

---
+++ Services/FakePayment/Services.FakePayment/Controllers/FakePaymentsController.cs
@@ -0,0 +1,18 @@
+﻿using Education.Shared.ControllerBases;
+using Education.Shared.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Services.FakePayment.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FakePaymentsController : CustomBaseController
+    {
+
+        [HttpPost]
+        public async Task<IActionResult> ReceivePayment()
+        {
+            return CreateActionResultInstance<NoContent>(Response<NoContent>.Success(200));
+        }
+    }
+}
