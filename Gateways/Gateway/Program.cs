ee0e3b2c278d7b57c3ccecb5b42891fcd7edd536
cevdet.timk <cevdet.timk@DT828>
2024-03-31 03:18:28 +0300

    Added gateway service

---
+++ Gateways/Gateway/Program.cs
@@ -0,0 +1,26 @@
+using Gateway.DelegateHandlers;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddJsonFile($"configuration.{builder.Environment.EnvironmentName.ToLower()}.json");
+
+
+builder.Services.AddHttpClient<TokenExhangeDelegateHandler>();
+builder.Services.AddAuthentication().AddJwtBearer("GatewayAuthenticationScheme", options =>
+{
+    options.Authority = builder.Configuration["IdentityServerURL"];
+    options.Audience = "resource_gateway";
+    options.RequireHttpsMetadata = false;
+});
+
+builder.Services.AddOcelot().AddDelegatingHandler<TokenExhangeDelegateHandler>();
+
+var app = builder.Build();
+
+app.UseAuthorization();
+app.UseDeveloperExceptionPage();
+app.MapControllers();
+await app.UseOcelot();
+app.Run();
--- IdentityServer/Education.IdentityServer/Config.cs
+++ IdentityServer/Education.IdentityServer/Config.cs
@@ -19,8 +19,8 @@ namespace EducationMicroService.IdentityServer
             new ApiResource("resource_discount"){Scopes={"discount_fullpermission"}},
             new ApiResource("resource_order"){Scopes={"order_fullpermission"}},
             new ApiResource("resource_payment"){Scopes={"payment_fullpermission"}},
-                  //              new ApiResource("resource_gateway"){Scopes={"gateway_fullpermission"}},
-                                 new ApiResource(IdentityServerConstants.LocalApi.ScopeName)
+            new ApiResource("resource_gateway"){Scopes={"gateway_fullpermission"}},
+            new ApiResource(IdentityServerConstants.LocalApi.ScopeName)
       };
 
         public static IEnumerable<IdentityResource> IdentityResources =>
@@ -46,8 +46,10 @@ namespace EducationMicroService.IdentityServer
                     new ApiScope("order_fullpermission","Order API için full erişim"),
                     
                     new ApiScope("payment_fullpermission","Payment API için full erişim"),
-                     //              new ApiScope("gateway_fullpermission","Gateway API için full erişim"),
-                new ApiScope(IdentityServerConstants.LocalApi.ScopeName)
+
+                    new ApiScope("gateway_fullpermission","Gateway API için full erişim"),
+
+                    new ApiScope(IdentityServerConstants.LocalApi.ScopeName)
                 };
 
         public static IEnumerable<Client> Clients =>
